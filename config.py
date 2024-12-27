847ed48b627c5d16f0f3605b5725763656bb7a5b
Teza01x <110857514+teza01x@users.noreply.github.com>
2024-12-27 15:14:08 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,10 @@
 import os
 
-telegram_token = "6394401969:AAEjPyyxttk484rOgt06tdPkEP2o6XtJH0g"
+telegram_token = ""
 
 data_base = os.path.join(os.path.dirname(__file__), 'database.db')
-admin_id = 5211056234
-channel_id = -1002123798460
-
-# admin_id = 603247380
+admin_id = 0111111
+channel_id = -1111111
 
 start_menu_status = 0
 
